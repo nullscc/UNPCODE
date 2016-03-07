3ea73cc8bbd97e1ce1908db6fb0e66875ccda3b9
ubuntu <ubuntu@baby.(none)>
2016-03-07 16:45:26 +0800

    add time_get folder

---
+++ time_get/clitime.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <sys/types.h> 
+ #include <netinet/in.h>
+ 
+int main()
+{
+		int sock_fd, n;
+		int ret;
+		char recieve[100];
+		struct sockaddr_in time_addr;
+		time_addr.sin_family = AF_INET;
+		time_addr.sin_port = htons(13);
+		inet_aton("127.0.0.1", &time_addr.sin_addr);
+		
+		if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		{
+			printf("create socket failed\n");
+			return -1;	
+		}
+	
+		if((ret = connect(sock_fd, (const struct sockaddr*)&time_addr, sizeof(struct sockaddr_in))) == -1)
+		{
+			printf("connect error\n");	
+			return -1;
+		}
+		
+		while((n = read(sock_fd, recieve, 100)) > 0)
+		{
+			recieve[n] = 0;
+			if(fputs(recieve, stdout) == EOF)	
+			{
+				printf("error fputs\n");	
+			}
+		}
+		
+		if(n<0)
+		{
+				printf("read error\n");
+		}
+		return 0;
+}
---
+++ time_get/servtime.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <time.h>
+#include <string.h>
+#include <netinet/in.h>
+
+int main()
+{
+	int sockfd;
+	int Connetedfd;
+	char buff[100];
+	struct sockaddr_in servaddr; //在netinet/in.h中定义
+	time_t ticks;
+	if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 )
+	{
+		printf("create socket error\n");	
+		return -1;
+	}
+	
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(13);
+	inet_aton("127.0.0.1", &servaddr.sin_addr);
+	
+	if( bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
+	{
+		printf("bind error\n");	
+		return -1;
+	}
+	
+	if( listen(sockfd, 10) == -1)
+	{
+		printf("listen failed\n");
+		return -1;	
+	}
+	
+	for( ; ;)
+	{
+		Connetedfd = accept(sockfd, NULL, NULL);
+		ticks = time(NULL);
+		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+		write(Connetedfd, &buff, strlen(buff));
+		close(Connetedfd);
+		break;
+	}
+	
+	return 0;
+}
