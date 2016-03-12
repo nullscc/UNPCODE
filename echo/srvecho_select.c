c558d150fecee664de5141ad173672c985a92071
xiezw <jarves@foxmail.com>
2016-03-12 16:17:38 +0800

    add file which forgetted

---
+++ echo/srvecho_select.c
@@ -0,0 +1,87 @@
+#include <zwunp.h>
+
+
+int main()
+{
+	int listenfd, connfd;
+	struct sockaddr_in srvaddr;
+	int maxi, maxfd, nready, i, n, client[FD_SETSIZE];
+	fd_set srvrdset;
+	char buf[4096];
+	
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	srvaddr.sin_family = AF_INET;
+	srvaddr.sin_port = htons(6677);
+	srvaddr.sin_addr.s_addr = INADDR_ANY;
+
+	Bind(listenfd, (SA *)&srvaddr, sizeof(srvaddr));
+
+	Listen(listenfd, 100);
+
+	for(i=0; i<FD_SETSIZE; i++)
+	{
+		client[i] = -1;
+	}
+	client[0] = listenfd;
+	maxi = 0;
+	maxfd = listenfd;
+	FD_ZERO(&srvrdset);
+	for(;;)
+	{
+		for(i=0; i<=maxi; i++)
+		{
+			if(client[i] != -1)
+				FD_SET(client[i], &srvrdset);
+		}
+		nready = Select(maxfd+1, &srvrdset, NULL, NULL, NULL);
+		for(i=0; i <= maxi; i++)
+		{
+			if(nready == 0)
+			{	
+				break;
+			}
+			
+			if( FD_ISSET(client[i], &srvrdset) )
+			{
+				if(listenfd == client[i])
+				{
+					connfd = Accept(listenfd, NULL, NULL);
+					nready--;
+					if(connfd > maxfd)
+						maxfd= connfd;
+					for(i=0; i<FD_SETSIZE; i++)
+					{
+						if(client[i] == -1)
+						{
+							if(i>maxi)
+								maxi=i;
+							client[i] = connfd;
+							break;
+						}
+					}
+					
+				}
+				else
+				{
+					nready--;
+					if( (n = Read(client[i], buf, 4096)) == 0 )
+					{
+						FD_CLR(client[i], &srvrdset);
+						close(client[i]);
+						client[i] = -1;
+						continue;
+					}
+					for(i=1; i<=maxi; i++)
+					{	
+						if(client[i] != -1)
+						Writen(client[i], buf, strlen(buf));
+					}
+					memset(buf, 0, 4096);
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
