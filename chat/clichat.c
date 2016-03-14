60936ab963ca1b0e7062bcde4806896dc5b096b5
xiezw <jarves@foxmail.com>
2016-03-14 20:39:04 +0800

    add chat folder

---
+++ chat/clichat.c
@@ -0,0 +1,87 @@
+#include "zwunp.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void strcli_select(FILE* fp, int fd, struct chat_info *msginfo)
+{
+	char buf[MAXLINE];
+	struct chat_info rcvinfo;
+	fd_set sel_rdset;
+	int maxfd;
+	int stdineof = 0;
+
+	FD_ZERO(&sel_rdset);
+	for(;;)
+	{
+		FD_SET(fileno(fp), &sel_rdset);
+		FD_SET(fd, &sel_rdset);
+		maxfd = max(fileno(fp), fd) + 1;
+		Select(maxfd, &sel_rdset, NULL, NULL, NULL);
+		if(FD_ISSET(fd, &sel_rdset))
+		{
+			if( Read(fd, &rcvinfo, sizeof(struct chat_info)) == 0)
+			{
+				if(stdineof == 0)
+				{
+					printf("server terminted prematurely\n");
+				}
+				return;
+			}
+			printf("%s      %s\n", rcvinfo.UserName, rcvinfo.RealTime);
+			printf("%s\n", rcvinfo.msg);
+
+			memset(&rcvinfo, 0, sizeof(struct chat_info));
+
+		}
+		if(FD_ISSET(fileno(fp), &sel_rdset))
+		{
+			if( Read(fileno(fp), buf, MAXLINE) == 0)
+			{
+					stdineof = 1;
+					shutdown(fd, SHUT_WR);
+					FD_CLR(fileno(fp), &sel_rdset);
+					continue;
+			}
+			if( (buf[0] == '\n')  )
+				continue;
+			memset(msginfo->msg, 0, MAXLINE);
+			memcpy(msginfo->msg, buf, strlen(buf));
+			Writen(fd, msginfo, sizeof(struct chat_info) - (MAXLINE-strlen(buf)));
+			memset(buf, 0, MAXLINE);
+			memset(msginfo->msg, 0, MAXLINE);
+		}
+
+	}
+
+}
+
+
+int main(int argc, char**argv)
+{
+	int sockfd;
+	struct sockaddr_in srvaddr;
+	struct chat_info cli_info;
+		
+
+	if(argc != 2)
+	{
+		printf("Usage:%s <IPAdress>\n", argv[0]);
+		return -1;
+	}
+	memset(&cli_info, 0, sizeof(struct chat_info));
+	memcpy(cli_info.UserName, "baby", 25);
+
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	srvaddr.sin_family = AF_INET;
+	srvaddr.sin_port = htons(6677);
+	inet_aton(argv[1], &srvaddr.sin_addr);
+
+	Connect(sockfd, (SA*)&srvaddr, sizeof(srvaddr));
+
+	strcli_select(stdin, sockfd, &cli_info);
+
+
+	return 0;
+}
---
+++ chat/srvchat.c
@@ -0,0 +1,112 @@
+#include "zwunp.h"
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define OPEN_MAX 256
+
+int main()
+{
+	int listenfd, connfd;
+	struct sockaddr_in srvaddr, cliaddr;
+	int maxi, nready, i, n;
+	struct pollfd clipolfd[OPEN_MAX];
+
+	struct chat_info cli_info;
+	time_t ticks;
+
+	memset(&cli_info, 0, sizeof(struct chat_info));
+
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	srvaddr.sin_family = AF_INET;
+	srvaddr.sin_port = htons(6677);
+	srvaddr.sin_addr.s_addr = INADDR_ANY;
+
+	Bind(listenfd, (SA *)&srvaddr, sizeof(srvaddr));
+
+	Listen(listenfd, 100);
+
+	for(i=0; i<OPEN_MAX; i++)
+	{
+		clipolfd[i].fd = -1;
+	}
+	
+	clipolfd[0].fd     = listenfd;
+	clipolfd[0].events = POLLRDNORM;
+	
+	maxi = 0;
+
+	for(;;)
+	{
+		nready = Poll(clipolfd, maxi+1, INFTIM);
+		for(i=0; i <= maxi; i++)
+		{
+			if(nready == 0)
+			{	
+				break;
+			}
+			if( clipolfd[i].revents & (POLLRDNORM | POLLERR))
+			{
+				if(listenfd == clipolfd[i].fd)
+				{
+					socklen_t len;
+					len = sizeof(cliaddr);
+					connfd = Accept(listenfd, (SA *)&cliaddr, &len);
+					printf("%s:%d connected\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+					nready--;
+					for(i=0; i<OPEN_MAX; i++)
+					{
+						if(clipolfd[i].fd == -1)
+						{
+							if(i>maxi)
+								maxi=i;
+							clipolfd[i].fd     = connfd;
+							clipolfd[i].events = POLLRDNORM;
+							break;
+						}
+					}
+					
+				}
+				else
+				{
+					nready--;
+					if( (n = Read(clipolfd[i].fd, &cli_info, sizeof(struct chat_info))) <= 0 )
+					{
+						if( (n < 0) && (errno == ECONNRESET) )
+						{
+							printf("client[%d] has aborted the connection\n", i);
+							continue;
+						}
+						else if(n < 0)
+							exit(1);
+						if(n == 0)
+						{
+							printf("client[%d] has terminted the connection\n", i);
+							close(clipolfd[i].fd);
+							clipolfd[i].fd = -1;
+							continue;
+						}
+					}
+					for(i=1; i<=maxi; i++)
+					{	
+						if(clipolfd[i].fd != -1)
+						{
+
+							ticks = time(NULL);
+							snprintf(cli_info.RealTime, sizeof(cli_info.RealTime), "%.24s", ctime(&ticks));
+							//memcpy(cli_info.RealTime, ctime(time(NULL)), );
+							Writen(clipolfd[i].fd, &cli_info, sizeof(struct chat_info) - (MAXLINE-strlen(cli_info.msg)));
+						}
+					}
+					memset(&cli_info, 0, sizeof(struct chat_info));
+					
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
+
