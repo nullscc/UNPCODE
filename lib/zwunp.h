74b01d9158444e77443333fad63d6c8a6dbfa9f2
xiezw <jarves@foxmail.com>
2016-03-15 22:16:59 +0800

    add Macro:container_of

--- lib/zwunp.h
+++ lib/zwunp.h
@@ -28,6 +28,12 @@
 
 #define	MAXLINE		4096	/* max text line length */
 
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
+#define container_of(ptr, type, member) ({      \
+    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+    (type *)( (char *)__mptr - offsetof(type,member) );})
+
 //typedef struct sockaddr SA;  // why error
 #define SA struct sockaddr
 typedef void Sigfunc(int);
